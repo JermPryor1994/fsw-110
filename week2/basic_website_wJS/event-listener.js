27b6e10f3d9162a3e672b9a902664792d70a7a1f
JermPryor1994 <jeremy.pryor@bryanuniversity.edu>
2022-01-11 18:51:46 -0800

    changes and corrections

--- week2/basic_website_wJS/event-listener.js
+++
@@ -1,7 +0,0 @@
-var btn1 = document.getElementById('btn1');
-var par1 = document.getElementById('par1');
-btn1.addEventListener('click', function(){
-    par1.innerHTML = 'Hello, Jeremy Pryor!';
-});
-
-
--- week2/event-listener/event-listener.js
+++ week2/event-listener/event-listener.js
@@ -1,5 +1,5 @@
-var btn1 = document.getElementById(btn1);
+var btn1 = document.getElementById('btn1');
+var par1 = document.getElementById('par1')
 btn1.addEventListener('click', function(){
-    console.log('Hello, Jeremy Pryor!');
-    document.getElementById('par1').innerHTML = 'Hello, Jeremy Pryor!';
+    par1.innerHTML = 'Hello, Jeremy Pryor!';
 });
