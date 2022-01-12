7ff2f2779c38d42ad147d3ac1a2fbb7973f974e4
JermPryor1994 <jeremy.pryor@bryanuniversity.edu>
2022-01-11 18:48:00 -0800

    event listener quick exercise

---
+++ week2/basic_website_wJS/event-listener.js
@@ -0,0 +1,7 @@
+var btn1 = document.getElementById('btn1');
+var par1 = document.getElementById('par1');
+btn1.addEventListener('click', function(){
+    par1.innerHTML = 'Hello, Jeremy Pryor!';
+});
+
+
