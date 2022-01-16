fc762d3327ea733a8af91a8b63395f68ad20c607
JermPryor1994 <jeremy.pryor@bryanuniversity.edu>
2022-01-16 13:05:43 -0800

    create-a-list completed assignment

---
+++ week3/create_a_list/create_a_list.js
@@ -0,0 +1,18 @@
+var newH1 = document.createElement('h1');
+newH1.textContent = 'Hello World';
+document.body.appendChild(newH1);
+
+for (var i = 0; i <= 9; i++){
+    var addH1 = document.createElement('h1');
+    addH1.textContent = 'Hello World'
+    document.body.appendChild(addH1);
+    addH1.style.color = 'green'
+}
+
+const names = ['steve', 'larry', 'joe', 'shirley', 'steph', 'nate', 'rick', 'emily'];
+
+for (var i = 0; i < names.length; i++){
+    var displayName = document.createElement('h2');
+    displayName.textContent = names[i];
+    document.body.appendChild(displayName);
+}
