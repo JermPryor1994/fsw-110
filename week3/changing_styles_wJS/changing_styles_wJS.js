6cc07078c987c2584d589de1dfdde86aefe0c55e
JermPryor1994 <jeremy.pryor@bryanuniversity.edu>
2022-01-16 13:38:12 -0800

    changing_styles completed assignment

---
+++ week3/changing_styles_wJS/changing_styles_wJS.js
@@ -0,0 +1,10 @@
+for (i=0; i<5; i++){
+    var newH2 = document.createElement('h2');
+    newH2.textContent = 'Learning Is Fun!';
+    document.body.appendChild(newH2);
+    newH2.style.fontSize = '20px';
+    newH2.style.fontWeight = 'lighter';
+    newH2.style.fontFamily = 'sans-serif';
+    newH2.style.color = 'cornflowerblue';
+    newH2.className = 'border'
+}
