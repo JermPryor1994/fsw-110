8eb70756f2dcb9ed5364fbf5aca20b408a82104d
JermPryor1994 <jeremy.pryor@bryanuniversity.edu>
2022-01-29 13:25:00 -0800

    Submit Events completed assignment

---
+++ week5/submit-events/submit-events.js
@@ -0,0 +1,10 @@
+
+var button = document.getElementById('button');
+
+button.addEventListener('click', function(){
+    var input1 = document.getElementById('input1').value;
+    var input2 = document.getElementById('input2').value;
+    var input3 = document.getElementById('input3').value;
+    alert(input1 + " " + input2 + " " + input3);
+})
+
